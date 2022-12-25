f4889f2f09f551bbdd7d9ea6b53c032754c9ed1d
Alexandr Rybalko <alexmobilepost@gmail.com>
2022-12-25 18:37:36 +0300

    class Database

--- db.py
+++ db.py
@@ -1,35 +1,29 @@
 import psycopg2
 from psycopg2 import Error
 
+class Database:
+    def __init__(self):
+        self.connection = None
+        self.cursor = None
+        self.create_connection()
 
-def create_connection(articles_list: list):
-    connection = None
-    cursor = None
-
-    try:
-        # Подключение к существующей базе данных
-        connection = psycopg2.connect(user="postgres",
-                                      # пароль, который указали при установке PostgreSQL
-                                      password="alexandr999",
-                                      host="127.0.0.1",
-                                      port="5432",
-                                      database="postgres")
-
-        # Курсор для выполнения операций с базой данных
-        cursor = connection.cursor()
-
-        cursor.execute("INSERT INTO ARTICLE (article_id) VALUES (%s)", ['127897897893456', ])
-
-        connection.commit()
-        cursor.execute("SELECT * from ARTICLE")
-        print("Результат", cursor.fetchall())
-    except (Exception, Error) as error:
-        print("Ошибка при работе с PostgreSQL", error)
-    finally:
-        if connection:
-            cursor.close()
-            connection.close()
+    def __del__(self):
+        if self.connection:
+            self.cursor.close()
+            self.connection.close()
             print("Соединение с PostgreSQL закрыто")
 
+    def create_connection(self):
+        try:
+            # Подключение к существующей базе данных
+            self.connection = psycopg2.connect(user="postgres",
+                                          # пароль, который указали при установке PostgreSQL
+                                          password="alexandr999",
+                                          host="127.0.0.1",
+                                          port="5432",
+                                          database="postgres")
 
-create_connection([])
+            # Курсор для выполнения операций с базой данных
+            self.cursor = self.connection.cursor()
+        except (Exception, Error) as error:
+            print("Ошибка при работе с PostgreSQL", error)
